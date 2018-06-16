b3cb5ff044f224e14e2e054875ea6a67df683bc2
Srikanth Chenna <srikanthchenna543@gmail.com>
2018-06-16 13:06:24 +0530

    commit

---
+++ src/main/java/com/chenna/adminapp/AdminAppApplication.java
@@ -0,0 +1,12 @@
+package com.chenna.adminapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminAppApplication.class, args);
+	}
+}
